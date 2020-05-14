b383b00d83b1f317a1014e9d87a3890c1e5c9e00
SvenRei <sven.reimers@tuhh.de>
2020-05-14 13:34:18 +0200

    adding a killable contract to the RPS Game

---
+++ contracts/Killable.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.8;
+
+import '@openzeppelin/contracts/ownership/Ownable.sol';
+import '@openzeppelin/contracts/lifecycle/Pausable.sol';
+
+contract Killable is Ownable, Pausable{
+
+     bool private killed;
+
+    event LogKilled(address indexed sender);
+    event LogRefunds(address indexed sender, uint refunds);
+
+    modifier whenAlive{
+        require(!killed, "isKilled");
+        _;
+    }
+
+    modifier whenKilled{
+        require(killed, "isAlive");
+        _;
+    }
+
+    function isKilled() public view returns(bool _killed) {
+        return killed;
+    }
+
+    function kill() public onlyOwner whenAlive whenPaused {
+        killed = true;
+        emit LogKilled(msg.sender);
+    }
+
+    //https://solidity.readthedocs.io/en/v0.5.3/security-considerations.html#security-considerations
+    function safeFunds() external onlyOwner whenKilled{
+        require(address(this).balance != 0, "the balance of the contract can't be zero");
+        emit LogRefunds(msg.sender, address(this).balance);
+        msg.sender.transfer(address(this).balance);
+    }
+}
