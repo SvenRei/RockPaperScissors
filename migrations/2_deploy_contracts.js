1a7cb685da9b622687b89030aff449211237da30
SvenRei <sven.reimers@tuhh.de>
2020-05-14 13:37:58 +0200

    migration

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+//only deploy RockPaperScissors
+var RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
+
+module.exports = function(deployer) {
+
+  deployer.deploy(RockPaperScissors);
+  
+};
