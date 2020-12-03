d0a21adf92897310d0c1186dc46466296cd8f9a1
SvenRei <sven.reimers@tuhh.de>
2020-12-03 22:35:04 +0100

    adding version: "0.6.0"

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,17 @@
 module.exports = {
+
+  compilers: {
+    solc: {
+    version: "0.6.0",    // Fetch exact version from solc-bin (default: truffle's version)
+
+  }
+},
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
+  }
   }
 };
